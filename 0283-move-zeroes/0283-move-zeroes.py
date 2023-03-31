fa339e9b3c66e14b6db0bf4b1fc76776edc74d27
Yab112 <106923105+Yab112@users.noreply.github.com>
2023-03-31 14:14:03 +0300

    Time: 174 ms (49.88%), Space: 15.6 MB (51.63%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,40 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left ,right = 0,1   # intializing two pointrs at the same point
+        while right < len(nums) and left < right:
+            if nums[left] == 0 and nums[right]!=0:
+                nums[left] ,nums[right] = nums[right] ,nums[left]
+                right += 1
+                left += 1
+            elif nums[left] != 0 and nums[right]==0:
+                right += 1
+                left += 1
+            elif nums[right] != 0 and nums[left] != 0:
+                right += 1
+                left += 1
+            else:
+                right += 1
+            
+            
+        
+       
+    """" check out this stack code, it ain't work for this scenario but it helped me to understand what is stack and how to work with stack,by the way when i say it ain't work i am teeling you that this doesn't technically they want you to do """
+        # count = 0
+        # stack = []
+        # for i in nums:
+        #     stack.append(i)
+        #     print(stack)
+        #     if i == 0:
+        #         stack.pop()
+        #         print(stack)
+        #         count += 1
+        # for i in range(count):
+        #     stack.append(0)
+        #     print(stack)
+  
+      
+      
+            
+            
+            
+        
