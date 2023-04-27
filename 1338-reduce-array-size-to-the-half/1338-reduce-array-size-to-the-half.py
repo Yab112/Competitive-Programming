44fbe31e5647af347d529774c9b80a6574ed6814
Yab112 <106923105+Yab112@users.noreply.github.com>
2023-04-27 08:43:02 +0300

    Time: 584 ms (65.85%), Space: 38.3 MB (16.05%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -6,10 +6,10 @@ class Solution:
                 dic[i ] = 1
             else:
                 dic[i] += 1
-        h = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1])}
+        h = sorted(dic.items(), key=lambda item: item[1])
         differnce, count = 0, 0
         while differnce < len(arr)/2:
-            tmp = h.popitem()
+            tmp = h.pop()
             count += 1
             differnce += tmp[1]
         return count 
