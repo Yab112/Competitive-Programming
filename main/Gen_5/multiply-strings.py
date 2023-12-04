bee2d8614cf5b7f7dbcb05759c99fc9f75859a14
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2023-12-04 18:39:30 +0300

    Added solution for Multiply Strings

---
+++ main/Gen_5/multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num_1 = 0
+        num_2 = 0  
+        for i in num1:
+            num_1 =num_1*10 + ord(i) - ord('0')
+        for j in num2:
+            num_2 = num_2 * 10 + ord(j) - ord("0")
+        return str(num_1 * num_2 )
