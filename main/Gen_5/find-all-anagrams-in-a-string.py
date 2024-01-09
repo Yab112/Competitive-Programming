46ca20d330e12bd75c975bacbbb2bed2d3c214b4
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2024-01-09 17:42:14 +0300

    Added solution for Find All Anagrams in a String

--- main/Gen_5/find-all-anagrams-in-a-string.py
+++ main/Gen_5/find-all-anagrams-in-a-string.py
@@ -4,10 +4,15 @@ class Solution:
         p_c = Counter(p)
         l = 0
         r = len(p) - 1
+        k = len(p)
         indices = []
         while r < len(s):
             if Counter(s[l:r+1]) == p_c:
                 indices.append(l)
-            l += 1
-            r+= 1
+            if  r + 2 < len(s) and s[r+1] not in p_c:
+                l  = r + 2
+                r = (l + (k-1))
+            else:
+                l += 1
+                r+= 1
         return indices
