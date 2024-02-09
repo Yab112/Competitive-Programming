67fac458d02c8e1e81e23504563fed75003e94c4
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2024-02-09 10:51:21 +0300

    Add solution for Maximum Subarray

---
+++ leetcode/maximum-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        global_max = float("-inf")
+        local_max = 0
+        for i in range(len(nums)):
+            local_max = max(nums[i],nums[i] + local_max)
+            if local_max > global_max:
+                global_max = local_max
+        return global_max
+        
