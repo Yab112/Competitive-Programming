5a54b8d658f433babfc5eedc341c05be9f06e2e7
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2024-03-01 23:09:50 +0300

    Add solution for Trapping Rain Water

---
+++ leetcode/trapping-rain-water.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def __init__(self):
+        self.mono_stack = []
+        self.res = 0
+
+    def trap(self, height: List[int]) -> int:
+        def l_r_max(nums):
+            res = [0] * len(nums)
+            num = nums[0]
+            for i in range(1,len(nums)):
+                res[i] = num
+                num = max(num,nums[i])
+            return res  
+        l_max = l_r_max(height)
+        r_max = l_r_max(height[::-1])[::-1]
+        return sum([min(l_max[i],r_max[i]) - height[i] for i in range(len(height)) if min(l_max[i],r_max[i]) - height[i] >= 0])
+        
