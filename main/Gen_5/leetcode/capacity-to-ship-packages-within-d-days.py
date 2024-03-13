a4da8034313de78b76125ae38f0370415440bfbb
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2024-03-13 22:48:20 +0300

    Add solution for Capacity To Ship Packages Within D Days

--- main/Gen_5/leetcode/capacity-to-ship-packages-within-d-days.py
+++ main/Gen_5/leetcode/capacity-to-ship-packages-within-d-days.py
@@ -1,25 +1,24 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        l = max(weights)
-        r = sum(weights)
-        while l <= r:
-            mid = l + ( r - l)//2
-            if self.simulate_sum(weights,mid) > days:
-                l = mid + 1
+        min_w = max(weights)
+        max_w = sum(weights)
+        while min_w < max_w:
+            mid_w = min_w + (max_w - min_w) // 2
+            if self.predict(weights, mid_w, days):
+                max_w = mid_w
             else:
-                r = mid - 1
-        return l
-
-    def simulate_sum(self,w,mid_w):
-        l = res = day = 0
-        for i in w:
-            if i + res <= mid_w:
-                res  += i
+                min_w = mid_w + 1
+        return min_w
+    
+    def predict(self, weights, mid_w, days):
+        l = r = 0
+        res = 0
+        day = 1
+        while r < len(weights):
+            if res + weights[r] <= mid_w:
+                res += weights[r]
             else:
                 day += 1
-                res = i 
-        return day + 1
-            
-
-
-
+                res = weights[r]
+            r += 1
+        return day <= days
