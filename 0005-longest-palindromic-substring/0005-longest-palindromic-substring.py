95626147a053ec89bd74afeb09f594fc1b23c279
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2025-03-29 09:31:20 +0300

    Time: 259 ms (81.57%), Space: 17.8 MB (84.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,20 +1,22 @@
 class Solution:  
     def longestPalindrome(self, s: str) -> str:  
-        n = len(s)
-        if n <= 1:return s
-        start,end = 0,0
-        def expand_to_left_right(left,right)->tuple:
-            while left >= 0 and right < n and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return left + 1,right - 1
-        
-        for i in range(n):
-            left1,right1 = expand_to_left_right(i,i)
-            left2,right2 = expand_to_left_right(i,i+1)
+        if len(s) <= 1:  
+            return s   
+        start, end = 0, 0  
+        def expand_around_center(left: int, right: int)->tuple:  
+            while left >= 0 and right < len(s) and s[left] == s[right]:  
+                left -= 1  
+                right += 1  
+            return left + 1, right - 1  
+        for i in range(len(s)):  
+            # Check for odd-length palindromes  
+            left1, right1 = expand_around_center(i, i)  
+            # Check for even-length palindromes  
+            left2, right2 = expand_around_center(i, i + 1)  
 
-            if right1 - left1  > end - start :
-                start ,end = left1,right1
-            elif right2 - left2 > end- start:
-                start ,end = left2,right2
-        return s[start:end + 1]
+            if right1 - left1 > end - start:  
+                start, end = left1, right1  
+            if right2 - left2 > end - start:  
+                start, end = left2, right2  
+        
+        return s[start:end + 1] 
